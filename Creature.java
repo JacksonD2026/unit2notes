cc6f2bcdb3f37f29c842c914ade6bcbe6d81fbca
jackson <jackson.dobson2026@gbwl.org>
2024-09-19 12:56:15 +0000

    created creature class

---
+++ Creature.java
@@ -0,0 +1,23 @@
+// Some Java CLASSES define OBJECTS
+// These classes are like blueprints or templates
+// You can create OBJECTS from a CLASS like this 
+// OBJECTS are INSTANCSE of a certain class
+public class Creature{
+    // INSTANCE VARIABLES
+    // every Creature has certain attributes
+    String species;
+    double weight;
+    int numLegs;
+    boolean canPet;
+
+    //METHODS
+    // Every creature has certain behaviors
+    public void move() {
+
+    }
+    public void play() {
+
+    }
+
+
+}
